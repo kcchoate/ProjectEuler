02340c8d057f21564fd353f99026f39effd71ae3
Kendrick Choate <kendrickchoate@gmail.com>
2015-11-01 22:40:30 -0600

    Updated answer to 13. Not working, but close I think

--- Python/Problem13.py
+++ Python/Problem13.py
@@ -1,7 +1,15 @@
-﻿file = open("Problem13.txt", "r")
-print (file.readline())
+﻿'''This does not work as is. I suspect it is because of an overflow,
+thought I am not certain on many things in Python at this point'''
+
+file = open("Problem13.txt", "r")
 numbers = []
-for number in numbers:
-	number.append(file.readline())
-print (numbers)
+print ("Hi!")
+for i in range(0, 50):#save numbers into a list
+	numbers.append(int(file.readline()))
+
+total = 0.0
+for i in range(0, 50):#sum numbers
+    total += numbers[i]
+
+print (str(total)[:10])
 file.close()
